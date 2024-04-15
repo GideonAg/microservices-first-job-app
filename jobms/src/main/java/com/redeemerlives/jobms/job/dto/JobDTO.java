646fdcc9532ff6ef5fe40445d49655417dfb0b79
GideonAg <gideonagbosu72@gmail.com>
2024-04-15 15:34:32 +0000

    Added job-service

---
+++ jobms/src/main/java/com/redeemerlives/jobms/job/dto/JobDTO.java
@@ -0,0 +1,81 @@
+package com.redeemerlives.jobms.job.dto;
+
+import com.redeemerlives.jobms.job.external.Company;
+import com.redeemerlives.jobms.job.external.Review;
+
+import java.util.List;
+
+public class JobDTO {
+    private int id;
+    private String title;
+    private String description;
+    private Long minSalary;
+    private Long maxSalary;
+    private String location;
+    private Company company;
+    private List<Review> review;
+
+    public List<Review> getReview() {
+        return review;
+    }
+
+    public void setReview(List<Review> review) {
+        this.review = review;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(Long minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public Long getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(Long maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
---
+++ jobms/src/main/java/com/redeemerlives/jobms/job/impl/JobServiceImpl.java
@@ -0,0 +1,77 @@
+package com.redeemerlives.jobms.job.impl;
+
+import com.redeemerlives.jobms.job.Job;
+import com.redeemerlives.jobms.job.JobRepository;
+import com.redeemerlives.jobms.job.JobService;
+import com.redeemerlives.jobms.job.clients.CompanyClient;
+import com.redeemerlives.jobms.job.clients.ReviewClient;
+import com.redeemerlives.jobms.job.dto.JobDTO;
+import com.redeemerlives.jobms.job.external.Company;
+import com.redeemerlives.jobms.job.external.Review;
+import com.redeemerlives.jobms.job.mapper.JobMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobServiceImpl implements JobService {
+    private final JobRepository jobRepository;
+    private final CompanyClient companyClient;
+    private final ReviewClient reviewClient;
+
+    public JobServiceImpl(JobRepository jobRepository, CompanyClient companyClient, ReviewClient reviewClient) {
+        this.jobRepository = jobRepository;
+        this.companyClient = companyClient;
+        this.reviewClient = reviewClient;
+    }
+
+    @Override
+    public List<JobDTO> getAllJobs() {
+        List<Job> jobs = jobRepository.findAll();
+
+        return jobs.stream().map(this::convertJobAndCompanyToDTO).toList();
+    }
+
+    private JobDTO convertJobAndCompanyToDTO(Job job) {
+        int companyId = job.getCompanyId();
+        Company company = companyClient.getCompanyById(companyId);
+        List<Review> companyReviews = reviewClient.getReviews(companyId);
+
+        return JobMapper.mapJobWithCompany(job, company, companyReviews);
+    }
+
+    @Override
+    public JobDTO getJobById(String id) {
+        Optional<Job> job = jobRepository.findById(id);
+        return job.map(this::convertJobAndCompanyToDTO).orElse(null);
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobRepository.save(job);
+    }
+
+    @Override
+    public Boolean deleteJobById(String id) {
+        Optional<Job> job = jobRepository.findById(id);
+        job.ifPresent(jobRepository::delete);
+        return job.isPresent();
+    }
+
+    @Override
+    public boolean updateJob(String id, Job jobUpdate) {
+        Optional<Job> job = jobRepository.findById(id);
+        if (job.isPresent()) {
+            job.get().setTitle(jobUpdate.getTitle());
+            job.get().setDescription(jobUpdate.getDescription());
+            job.get().setMinSalary(jobUpdate.getMinSalary());
+            job.get().setMaxSalary(jobUpdate.getMaxSalary());
+            job.get().setLocation(jobUpdate.getLocation());
+            jobRepository.save(job.get());
+            return true;
+        }
+
+        return false;
+    }
+}
